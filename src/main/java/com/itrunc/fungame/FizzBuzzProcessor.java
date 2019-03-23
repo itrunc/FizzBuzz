aebf805c5a9a6a4ab1cfff5a6a8d6c9fa46e1574
panben <gomac@qq.com>
2019-03-23 16:11:44 +0800

    print Fizz Buzz in processor

--- src/main/java/com/itrunc/fungame/FizzBuzzProcessor.java
+++ src/main/java/com/itrunc/fungame/FizzBuzzProcessor.java
@@ -1,8 +1,39 @@
 package com.itrunc.fungame;
 
+import com.itrunc.fungame.conversion.BuzzCaculator;
+import com.itrunc.fungame.conversion.Caculator;
+import com.itrunc.fungame.conversion.FizzCaculator;
+import com.itrunc.fungame.model.NumberPlaceholder;
+
 public class FizzBuzzProcessor {
 
     public static void main(String[] args) {
-        System.out.println("Hello FizzBuzz");
+        FizzBuzzProcessor processor = new FizzBuzzProcessor();
+        try {
+            processor.process();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void process() throws Exception {
+        FizzCaculator fizzCaculator = new FizzCaculator();
+        BuzzCaculator buzzCaculator = new BuzzCaculator();
+
+        fizzCaculator.setSubCalculator(buzzCaculator);
+
+        Caculator<NumberPlaceholder> caculatorChain = fizzCaculator;
+
+        int loop = 1;
+        while (loop <= 100) {
+            NumberPlaceholder numberPlaceholder = new NumberPlaceholder(loop);
+            caculatorChain.caculate(numberPlaceholder);
+            if ("".equals(numberPlaceholder.placeholder)) {
+                System.out.println(numberPlaceholder.num);
+            } else {
+                System.out.println(numberPlaceholder.placeholder);
+            }
+            loop++;
+        }
     }
 }
